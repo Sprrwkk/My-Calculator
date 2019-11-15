4810477ab1a5b200c9dcaa768540808d88167f17
Sprrwkk <sprrwkk@gmail.com>
2019-11-15 15:25:58 +0100

    Create generalLayout object and _createDisplay function

--- my_calculator.py
+++ my_calculator.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QPushButton, QGridLayout
+from my_calc_view import my_calc_view
+from PyQt5.QtCore import Qt
+
 
 __version__ = 1.0
 __author__ = 'Łukasz Wróbel'
@@ -8,12 +11,30 @@ class CalculatorInterface(QMainWindow):
     """Calculator main interface class"""
     def __init__(self):
         # View initialization
+        self.generalLayout = my_calc_view()
         super().__init__()
         self.setWindowTitle(f"My Calc  v{__version__}")
-        self.setFixedHeight(350)
-        self.setFixedWidth(250)
+        #self.setFixedHeight(350)
+        #self.setFixedWidth(250)
+        self.setFixedSize(350, 250)
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+        self._createDisplay()
+        #self._createButtons()
+
+
+
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+
+
+
 
 
 
