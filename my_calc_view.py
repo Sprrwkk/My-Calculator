27b50efc0553e2e76f79873abef75402378b792d
Sprrwkk <sprrwkk@gmail.com>
2019-11-15 15:25:09 +0100

    my_calc_view that return generalLayout object from QVBoxLayout class

---
+++ my_calc_view.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGridLayout, QLineEdit, QPushButton, QVBoxLayout
+
+
+def my_calc_view():
+    """Function that return generalLayout object"""
+    generalLayout = QVBoxLayout()
+    return generalLayout
+
+my_calc_view()
